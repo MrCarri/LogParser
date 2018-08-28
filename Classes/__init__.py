97d9ed78ac27e4f6698446c054aa8c05593a37cb
Alexandre Carrillo <alexandrecarrillo@gmail.com>
2018-08-28 10:26:36 +0200

    Delete __init__.py

--- Classes/__init__.py
+++
@@ -1,8 +0,0 @@
-#from classes.lparser_reader import lparser_reader  as reader
-#from classes.lparser_jobmanager import lparser_jobmanager as job_manager
-#from classes.lparser_writer import lparser_writer as writer
-
-
-import  classes.lparser_reader as reader
-import  classes.lparser_jobmanager as job_manager
-import classes.lparser_writer as writer
